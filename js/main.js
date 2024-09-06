9818725653929de4bb93d4b1257be66a922d4ddd
helder <100797030+Helder8a@users.noreply.github.com>
2024-09-06 08:14:02 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,16 +16,6 @@
     new WOW().init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.nav-bar').addClass('sticky-top');
-        } else {
-            $('.nav-bar').removeClass('sticky-top');
-        }
-    });
-
-
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -40,42 +30,80 @@
     });
 
 
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav: true,
-        navText: [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    // Team carousel
+    $(".team-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
-        margin: 24,
+        center: false,
         dots: false,
         loop: true,
+        margin: 50,
         nav: true,
         navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
+        responsiveClass: true,
         responsive: {
             0: {
                 items: 1
             },
-            992: {
+            768: {
                 items: 2
+            },
+            992: {
+                items: 3
             }
         }
     });
+
+
+    // Testimonial carousel
+
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+
+    // Fact Counter
+
+    $(document).ready(function () {
+        $('.counter-value').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 2000,
+                easing: 'easeInQuad',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    });
+
+
 
 })(jQuery);
 
